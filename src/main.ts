2f6e22e8340862ab9b6685045eda2849708f3513
phuctq32 <phuctq32@gmail.com>
2023-05-18 23:03:25 +0700

    create user schema

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.setGlobalPrefix('api');
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(8080);
 }
 bootstrap();
---
+++ src/schemas/user.schema.ts
@@ -0,0 +1,38 @@
+import { Document, HydratedDocument } from 'mongoose';
+import { Prop, SchemaFactory, Schema } from '@nestjs/mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema()
+export class User extends Document {
+  @Prop({
+    required: true,
+    unique: true,
+  })
+  email: string;
+
+  @Prop({
+    minlength: 4,
+  })
+  name: string;
+
+  @Prop()
+  password?: string;
+
+  @Prop({
+    default:
+      'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png',
+  })
+  avatar: string;
+
+  @Prop()
+  birthday?: Date;
+
+  @Prop({ default: false })
+  is_verified: boolean;
+
+  @Prop({ default: false })
+  is_registered_with_google: boolean;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
